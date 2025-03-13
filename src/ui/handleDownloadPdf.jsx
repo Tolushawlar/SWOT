e293409355118d50254fdd2a12528249d51d26ce
Tolushawlar <tolushawlar.it@gmail.com>
2025-03-13 09:32:15 +0100

    new design updates

--- src/ui/handleDownloadPdf.jsx
+++ src/ui/handleDownloadPdf.jsx
@@ -13,24 +13,29 @@ const handleDownloadPdf = (
 
   // Define gradient styles for SWOT sections
   const sectionStyles = {
-    Strength: { colorStart: [205, 165, 0], colorEnd: [255, 145, 0] }, // Orange gradient
+    Strength: { colorStart: [19, 26, 42], colorEnd: [99, 87, 265] }, // Orange gradient
+    // Strength: { colorStart: [205, 165, 0], colorEnd: [255, 145, 0] }, // Orange gradient
     // Weakness: { colorStart: [173, 216, 230], colorEnd: [100, 149, 237] }, // Light blue gradient
-    Weakness: { colorStart: [0, 0, 139], colorEnd: [25, 25, 112] }, // Deep blue gradient
-    Opportunity: { colorStart: [128, 0, 128], colorEnd: [75, 0, 130] }, // Purple gradient
-    Threat: { colorStart: [0, 100, 0], colorEnd: [0, 128, 0] }, // Deep green gradient
+    // Weakness: { colorStart: [0, 0, 139], colorEnd: [25, 25, 112] }, // Deep blue gradient
+    // Opportunity: { colorStart: [128, 0, 128], colorEnd: [75, 0, 130] }, // Purple gradient
+    // Threat: { colorStart: [0, 100, 0], colorEnd: [0, 128, 0] }, // Deep green gradient
+    Weakness: { colorStart: [19, 26, 42], colorEnd: [99, 87, 265] }, // Orange gradient
+    Opportunity: { colorStart: [19, 26, 42], colorEnd: [99, 87, 265] }, // Orange gradient
+    Threat: { colorStart: [19, 26, 42], colorEnd: [99, 87, 265] }, // Orange gradient
+
   };
 
   let pageNumber = 1; // Track the current page number
 
   // Helper function to draw page border
   const drawPageBorder = (doc) => {
-    doc.setDrawColor(0, 0, 0); // Black border color
+    doc.setDrawColor(256, 256, 256); // Black border color
     doc.setLineWidth(1.5); // Border thickness
     doc.roundedRect(5, 5, 200, 287, 10, 10, "S"); // Rounded border with a radius of 10
   };
 
   // Helper function to add a soft background
-  const addSoftBackground = (doc, color = [22, 0, 57]) => {
+  const addSoftBackground = (doc, color = [99, 87, 165]) => {
     doc.setFillColor(...color);
     doc.rect(0, 0, 210, 297, "F"); // Cover full A4 page with the color
   };
@@ -65,7 +70,7 @@ const handleDownloadPdf = (
   // Set up for the second title
   doc.setFont("helvetica", "bold");
   doc.setFontSize(25);
-  doc.setTextColor(236, 66, 244); // White text
+  doc.setTextColor(19, 26, 42); // White text
 
   // Centering "SWOT ANALYSIS" title
   const title2 = user + " SWOT Analysis Result";
@@ -206,107 +211,107 @@ const handleDownloadPdf = (
 
   //   currentY += 20;
   // };
-//   const addSwotSection = (title, list, color) => {
-//     if (list.length === 0) return;
+  //   const addSwotSection = (title, list, color) => {
+  //     if (list.length === 0) return;
 
-//     // Start a new page for each section
-//     if (pageNumber > 1) {
-//         doc.addPage();
-//         addSoftBackground(doc);
-//         drawPageBorder(doc);
-//     }
-//     addFooter(doc);
-//     pageNumber += 1;
+  //     // Start a new page for each section
+  //     if (pageNumber > 1) {
+  //         doc.addPage();
+  //         addSoftBackground(doc);
+  //         drawPageBorder(doc);
+  //     }
+  //     addFooter(doc);
+  //     pageNumber += 1;
 
-//     let currentY = 30; // Reset Y position for new page
+  //     let currentY = 30; // Reset Y position for new page
 
-//     // Add section header
-//     doc.setDrawColor(0, 0, 0);
-//     doc.setLineWidth(0.5);
-//     doc.setFillColor(...color);
-//     doc.roundedRect(10, currentY, 190, 20, 10, 10, "FD");
+  //     // Add section header
+  //     doc.setDrawColor(0, 0, 0);
+  //     doc.setLineWidth(0.5);
+  //     doc.setFillColor(...color);
+  //     doc.roundedRect(10, currentY, 190, 20, 10, 10, "FD");
 
-//     doc.setFont("helvetica", "bold");
-//     doc.setFontSize(18);
-//     doc.setTextColor(255, 255, 255);
-//     doc.text(title, 15, currentY + 12);
+  //     doc.setFont("helvetica", "bold");
+  //     doc.setFontSize(18);
+  //     doc.setTextColor(255, 255, 255);
+  //     doc.text(title, 15, currentY + 12);
 
-//     currentY += 35;
+  //     currentY += 35;
 
-//     // Loop through results
-//     list.forEach((result) => {
-//         const { question, selected, response } = result;
+  //     // Loop through results
+  //     list.forEach((result) => {
+  //         const { question, selected, response } = result;
 
-//         doc.setFillColor(...color);
-//         doc.roundedRect(15, currentY, 180, 33, 10, 10, "F");
+  //         doc.setFillColor(...color);
+  //         doc.roundedRect(15, currentY, 180, 33, 10, 10, "F");
 
-//         doc.setFontSize(10);
-//         doc.setFont("helvetica", "bold");
-//         doc.setTextColor(255, 255, 255);
-//         doc.text(doc.splitTextToSize(`${question}:`, 170), 20, currentY + 7);
-//         doc.setFont("helvetica", "normal");
-//         doc.text(
-//             doc.splitTextToSize(`Selected: ${selected}`, 170),
-//             20,
-//             currentY + 15
-//         );
-//         doc.text(
-//             doc.splitTextToSize(`Response: ${response}`, 170),
-//             20,
-//             currentY + 22
-//         );
+  //         doc.setFontSize(10);
+  //         doc.setFont("helvetica", "bold");
+  //         doc.setTextColor(255, 255, 255);
+  //         doc.text(doc.splitTextToSize(`${question}:`, 170), 20, currentY + 7);
+  //         doc.setFont("helvetica", "normal");
+  //         doc.text(
+  //             doc.splitTextToSize(`Selected: ${selected}`, 170),
+  //             20,
+  //             currentY + 15
+  //         );
+  //         doc.text(
+  //             doc.splitTextToSize(`Response: ${response}`, 170),
+  //             20,
+  //             currentY + 22
+  //         );
 
-//         currentY += 35;
+  //         currentY += 35;
 
-//         if (currentY > 260) {
-//             addFooter(doc);
-//             pageNumber += 1;
-//             doc.addPage();
-//             addSoftBackground(doc);
-//             drawPageBorder(doc);
-//             currentY = 20;
-//         }
-//     });
+  //         if (currentY > 260) {
+  //             addFooter(doc);
+  //             pageNumber += 1;
+  //             doc.addPage();
+  //             addSoftBackground(doc);
+  //             drawPageBorder(doc);
+  //             currentY = 20;
+  //         }
+  //     });
 
-//     addFooter(doc);
-// };
+  //     addFooter(doc);
+  // };
 
-const addSwotSection = (title, list, color) => {
-  if (list.length === 0) return;
+  const addSwotSection = (title, list, color) => {
+    if (list.length === 0) return;
 
-  // Ensure Strengths start on page 2
-  if (pageNumber === 1 && title === "Strengths") {
+    // Ensure Strengths start on page 2
+    if (pageNumber === 1 && title === "Strengths") {
       doc.addPage();
       pageNumber += 1;
-  }
+    }
 
-  // Start a new page for each section (except Strengths since it's already handled)
-  if (pageNumber > 1 && title !== "Strengths") {
+    // Start a new page for each section (except Strengths since it's already handled)
+    if (pageNumber > 1 && title !== "Strengths") {
       doc.addPage();
       pageNumber += 1;
-  }
+    }
 
-  addSoftBackground(doc);
-  drawPageBorder(doc);
-  addFooter(doc);
+    addSoftBackground(doc);
+    drawPageBorder(doc);
+    addFooter(doc);
 
-  let currentY = 30; // Reset Y position for the new page
+    let currentY = 30; // Reset Y position for the new page
 
-  // Add section header
-  doc.setDrawColor(0, 0, 0);
-  doc.setLineWidth(0.5);
-  doc.setFillColor(...color);
-  doc.roundedRect(10, currentY, 190, 20, 10, 10, "FD");
+    // Add section header
+    doc.setDrawColor(0, 0, 0);
+    doc.setLineWidth(0.5);
+    doc.setFillColor(...color);
+    doc.roundedRect(10, currentY, 190, 20, 10, 10, "FD");
 
-  doc.setFont("helvetica", "bold");
-  doc.setFontSize(18);
-  doc.setTextColor(255, 255, 255);
-  doc.text(title, 15, currentY + 12);
+    doc.setFont("helvetica", "bold");
+    doc.setFontSize(18);
+    doc.setTextColor(255, 255, 255);
+    doc.text(title, 15, currentY + 12);
 
-  currentY += 35;
+    currentY += 35;
 
-  // Loop through results
-  list.forEach((result) => {
+    // Loop through results
+    list.forEach((result) => {
       const { question, selected, response } = result;
 
       doc.setFillColor(...color);
@@ -318,30 +323,30 @@ const addSwotSection = (title, list, color) => {
       doc.text(doc.splitTextToSize(`${question}:`, 170), 20, currentY + 7);
       doc.setFont("helvetica", "normal");
       doc.text(
-          doc.splitTextToSize(`Selected: ${selected}`, 170),
-          20,
-          currentY + 15
+        doc.splitTextToSize(`Selected: ${selected}`, 170),
+        20,
+        currentY + 15
       );
       doc.text(
-          doc.splitTextToSize(`Response: ${response}`, 170),
-          20,
-          currentY + 22
+        doc.splitTextToSize(`Response: ${response}`, 170),
+        20,
+        currentY + 22
       );
 
       currentY += 35;
 
       if (currentY > 260) {
-          addFooter(doc);
-          doc.addPage();
-          addSoftBackground(doc);
-          drawPageBorder(doc);
-          pageNumber += 1;
-          currentY = 20;
+        addFooter(doc);
+        doc.addPage();
+        addSoftBackground(doc);
+        drawPageBorder(doc);
+        pageNumber += 1;
+        currentY = 20;
       }
-  });
+    });
 
-  addFooter(doc);
-};
+    addFooter(doc);
+  };
 
 
   // Add SWOT sections
